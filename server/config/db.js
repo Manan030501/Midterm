70b89b891eeff89d3e146fbdd8f81d92a40b88f8
Manan030501 <72701544+Manan030501@users.noreply.github.com>
2020-10-30 21:57:31 -0400

    publis

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+
+//======Created by Manan========301107555
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://contact:EB1B7WjxpoU271Bi@cluster0.sz2wo.mongodb.net/books?retryWrites=true&w=majority"
+};
---
+++ server/models/books.js
@@ -0,0 +1,16 @@
+//======Created by Manan========301107555
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
